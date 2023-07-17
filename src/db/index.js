7340dd82a1423294d035e34b601b8d5871359433
DavidTMaciel <davidmacieljr122@gmail.com>
2023-07-17 20:41:52 -0300

    simulação do banco de dados

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    usuarios:[
+        {
+            id: 1,
+            nome: "Jose",
+            email: "jose@gmail.com",
+            telefone: "81 9844 1685",
+            perfil: 1
+        },
+        {
+            id:2,
+            nome:"lucas",
+            email:"lucas@gmail.com",
+            telefone:"81 9844 7888",
+            perfil: 2,
+        }
+    ],
+    perfis:[
+        {id: 1, descricao: "ADMIN"},
+        {id: 2, descricao: "NORMAL"},
+    ]
+}
